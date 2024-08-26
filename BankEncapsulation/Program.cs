670ea9ffbb93155964f1c3acd4c0dc5ccd2dff3b
Antonio Figueroa <antonio_figueroa@dell.com>
2024-08-26 15:25:23 -0500

    - completes encapsulation exercise

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount account = new BankAccount();
+            Console.WriteLine($"Your current balance is {account.GetBalance()}");
+            Console.WriteLine($"How much would you like to deposit?");
+            var depositString = Console.ReadLine();
+            bool result = double.TryParse(depositString, out double deposit);
+            if (result == false)
+            {
+                Console.WriteLine($"Sorry we couldn't complete your transaction. Please provide a ammount you want to deposit and try again.");
+            } else
+            {
+                account.Deposit(deposit);
+            }
+            Console.WriteLine($"Your current balance is {account.GetBalance()}");
         }
     }
 }
